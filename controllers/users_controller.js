39ed4022e423506f587d96d84f63d7bac81142a3
Ronak <ronakasnani5@gmail.com>
2021-04-16 00:38:07 +0530

    add forms

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,15 @@ module.exports.profile = function(req,res){
         title: "profile"
     })
 }
+// render signup page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up.ejs',{
+        title: "SocialBox | Sign Up"
+    })
+}
+// render sign in page
+module.exports.signIn = function(req,res){
+    return res.render("user_sign_in",{
+        title: "SocialBox | Sign Up"
+    })
+}
