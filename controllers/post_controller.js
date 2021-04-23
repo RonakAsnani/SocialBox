2d83faba66e8a554a57cdd8f39826973c3b409f3
Ronak <ronakasnani5@gmail.com>
2021-04-23 16:08:47 +0530

    add ajax

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -4,10 +4,19 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req,res){
     try{
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
+
+        if(req.xhr){
+            return res.status(200).json({
+                data:{
+                    post: post
+                },
+                message: "post created!"
+            })
+        }
         req.flash('success','Post published!')
         return res.redirect('back');
     }catch(err){
@@ -24,6 +33,16 @@ module.exports.destroy = async function(req,res){
         if(post.user == req.user.id){
             post.remove();
             await Comment.deleteMany({post:req.params.id});
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data:{
+                        post_id: req.params.id
+                    },
+                    message: "Post deleted "
+                })
+            }
+
             req.flash('success','Post deleted!')
             return res.redirect('back');
         }else{
