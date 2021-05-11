9aef58b4dfd593aa72796bb0ab74db2f5f35fd25
Ronak <ronakasnani5@gmail.com>
2021-05-11 21:44:13 +0530

    practice

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,97 +1,90 @@
-const User = require('../models/user');
+const User = require("../models/user");
 
-module.exports.profile = function(req,res){
-    if(req.cookies.user_id){
-        User.findById(req.cookies.user_id,function(err,user){
-            if(user){
-                return res.render('profile',{
-                    title: "User Profile",
-                    user: user
-                })
-            }
-            return res.redirect('/users/sign-in');
-        })
-    }else{
-        return res.redirect('/users/sign-in');
-    }
-}
+module.exports.profile = function (req, res) {
+  if (req.cookies.user_id) {
+    User.findById(req.cookies.user_id, function (err, user) {
+      if (user) {
+        return res.render("profile", {
+          title: "User Profile",
+          user: user,
+        });
+      }
+      return res.redirect("/users/sign-in");
+    });
+  } else {
+    return res.redirect("/users/sign-in");
+  }
+};
 // render signup page
-module.exports.signUp = function(req,res){
-    return res.render('user_sign_up',{
-        title: "SocialBox | Sign Up"
-    })
-}
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "SocialBox | Sign Up",
+  });
+};
 
 // render sign in page
-module.exports.signIn = function(req,res){
-    return res.render("user_sign_in",{
-        title: "SocialBox | Sign Up"
-    })
-}
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "SocialBox | Sign Up",
+  });
+};
 
 // sign out
-module.exports.signOut = function(req,res){
-    User.findOne({_email: req.body.email},function(err,user){
-        // user.cookies.remove();
-         return res.redirect('/users/sign-in');
-    })
-}
+module.exports.signOut = function (req, res) {
+  User.findOne({ _email: req.body.email }, function (err, user) {
+    // user.cookies.remove();
+    return res.redirect("/users/sign-in");
+  });
+};
 
 // get sign up data
-module.exports.create = function(req,res){
-    if(req.body.password != req.body.confirm_password){
-        return res.redirect('back');
+module.exports.create = function (req, res) {
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect("back");
+  }
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding user in signing up");
+      return;
     }
-    User.findOne({email : req.body.email},function(err,user){
-        if(err){
-            console.log("error in finding user in signing up"); 
-            return;
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log("error in creating user in signing up");
+          return;
         }
-        if(!user){
-            User.create(req.body,function(err,user){
-                if(err){
-                    console.log("error in creating user in signing up"); 
-                    return;
-                }
 
-                return res.redirect('/users/sign-in');
-            })
-        }else{
-            return res.redirect('back');
-        }
-    })
-
-}
+        return res.redirect("/users/sign-in");
+      });
+    } else {
+      return res.redirect("back");
+    }
+  });
+};
 
 //sign in and create a session for the user
-module.exports.createSession = function(req,res){
-// steps to authenticate
+module.exports.createSession = function (req, res) {
+  // steps to authenticate
 
-    //find the user
-    User.findOne({email:req.body.email},function(err,user){
-        if(err){
-            console.log("error in finding the user");
-            return ;
-        }
-         // handle user found
-         if(user){
-            // handle password which dont match
-            if(user.password != req.body.password){
-                return res.redirect('back');
-            }
-            // handle session creation
-            res.cookie('user_id',user.id);
-            return res.redirect('/users/profile');
-         }else{
-            // handle user not found
+  //find the user
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      console.log("error in finding the user");
+      return;
+    }
+    // handle user found
+    if (user) {
+      // handle password which dont match
+      if (user.password != req.body.password) {
+        return res.redirect("back");
+      }
+      // handle session creation
+      res.cookie("user_id", user.id);
+      return res.redirect("/users/profile");
+    } else {
+      // handle user not found
 
-            return res.redirect('back');
-         }
-    })
-   
-
-        
-
-    
-}
-
+      return res.redirect("back");
+    }
+  });
+};
--- index.js
+++ index.js
@@ -4,36 +4,49 @@ const app = express();
 const port = 8000;
 
 const expressLayouts = require("express-ejs-layouts");
-const db = require('./config/mongoose');
+const db = require("./config/mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passport-local-strategy");
 
 app.use(express.urlencoded());
 
-
 app.use(cookieParser());
 
-
-app.use(express.static('./assets'));
+app.use(express.static("./assets"));
 
 app.use(expressLayouts);
 // extract style and scripts form subpages into the layout
-app.set("layout extractStyles",true);
-app.set("layout extractScripts",true);
-
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 
 // use express router
-app.use('/',require('./routes'))
+app.use("/", require("./routes"));
 
 // setup the view engine
-app.set("view engine","ejs");
-app.set("views",'./views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
+app.use(
+  session({
+    name: "SocialBox",
+    secret: "oeiuboueibcnoiecbnoiuwhbhncoi",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+      maxAge: 1000 * 60 * 100,
+    },
+  })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.listen(port,function(err){
-    if(err){
-        console.log(`Error in running the server : ${err}`);
-        return;
-    }
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error in running the server : ${err}`);
+    return;
+  }
 
-    console.log(`Server is running on port: ${port}`);
-})
+  console.log(`Server is running on port: ${port}`);
+});
