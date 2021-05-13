247d26bb57962e5480134a1e2f2762bf9e29c313
Ronak <ronakasnani5@gmail.com>
2021-05-13 23:37:30 +0530

    feat: parallel jobs

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,6 +1,8 @@
 const Comment = require("../models/comment");
 const Post = require("../models/post");
 const commentsMailer = require("../mailers/comments_mailer");
+const queue = require("../config/kue");
+const commentEmailWorker = require("../workers/comment_email_worker");
 
 module.exports.create = async function (req, res) {
   try {
@@ -15,7 +17,14 @@ module.exports.create = async function (req, res) {
       post.comments.push(comment);
       post.save();
       comment = await comment.populate("user", "name email").execPopulate();
-      commentsMailer.newComment(comment);
+      // commentsMailer.newComment(comment);
+      let job = queue.create("emails", comment).save(function (err) {
+        if (err) {
+          console.log("error in creating queue", err);
+          return;
+        }
+        console.log(job.id, "job enqueued");
+      });
       if (req.xhr) {
         // comment = await comment.populate("user", "name").execPopulate();
 
